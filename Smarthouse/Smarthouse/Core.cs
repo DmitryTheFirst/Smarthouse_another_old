6fd758aee30637ac41045418cb295303f1494ffd
Smirnyaga <smirnyaga4@yandex.ru>
2013-12-28 22:16:23 +0400

    Created 6 new classes. A few comments

---
+++ Smarthouse/Smarthouse/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smarthouse
+{
+    class Core
+    {
+        //Download smth from cfgs
+        //Init other classes
+        //watching all's status, doing smth interesting
+    }
+}
--- Smarthouse/Smarthouse/Program.cs
+++ Smarthouse/Smarthouse/Program.cs
@@ -10,6 +10,7 @@ namespace Smarthouse
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            //init core
         }
     }
 }
