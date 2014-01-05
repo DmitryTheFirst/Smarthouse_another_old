519b2865afa542eeee67e20b40b46e8476968e6a
Smirnyaga <smirnyaga4@yandex.ru>
2014-01-05 05:58:58 +0400

    AddUser in UserDomain. 4 variants

--- Smarthouse/Smarthouse/UserDomain.cs
+++ Smarthouse/Smarthouse/UserDomain.cs
@@ -7,15 +7,46 @@ namespace Smarthouse
 {
     class UserDomain
     {
+        Dictionary<string, User> users = new Dictionary<string, User>();
+        enum Statuses : byte { None=0, Net=1 };
+        
+        public bool AddUser(string login, string pass, string about)
+        {
+            return AddUser(login,pass,new List<uint>(),new List<uint>(),about);
+        }
+        public bool AddUser(string login, string pass, List<uint> privileges, List<uint> subscriptions, string about)
+        {
+            return AddUser(login,pass,privileges,subscriptions,about,DateTime.MinValue);
+        }
+        public bool AddUser(string login, string pass, List<uint> privileges, List<uint> subscriptions, string about, DateTime lastLogin)
+        {
+            return AddUser(login, new User(pass, privileges, subscriptions, (byte)Statuses.None, about, lastLogin));
+        }
+        public bool AddUser(string login, User user)
+        {
+            users.Add(login, user);
+            return true;
+        }
 
+        //public bool r
     }
     class User
     {
-        string Pass;
-        List<uint> Privileges;
-        List<uint> Subscriptions;
-        byte Status;
-        string About;
-        DateTime LastLogin;
+        public string Pass { get; set; }
+        public List<uint> Privileges { get; set; }
+        public List<uint> Subscriptions { get; set; }
+        public byte Status { get; set; }
+        public string About { get; set; }
+        public DateTime LastLogin { get; set; }
+
+        public User(string pass, List<uint> privileges, List<uint> subscriptions, byte status, string about, DateTime lastLogin)
+        {
+            Pass = pass;
+            Privileges = privileges;
+            Subscriptions = subscriptions;
+            Status = status;
+            About = about;
+            LastLogin = lastLogin;
+        }
     }
 }
